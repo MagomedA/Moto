55c76c1ef56b24bab575d52b4df88a4c4d72803c
Magomed200121 <89939948+Magomed200121@users.noreply.github.com>
2022-03-21 13:11:33 +0300

    create

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$('.question__el').click(function() {
+  $(this).children('.question__el-text').slideToggle()
+})
+
+
+var swiper = new Swiper(".route__inner-img", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
